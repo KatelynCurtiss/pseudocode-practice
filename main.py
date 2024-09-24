9e388d6f733d163e36a9c141d25d9d4eae973cbe
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-24 11:05:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,33 @@
 
 # Practice A
 
+# Input 
+name = (input("Please enter your first name: "))
+quiz1 = int(input("Enter your first quiz score:"))
+quiz2 = int(input("Enter your second quiz score: "))
+quiz3 = int(input("Enter your third quiz score: "))
+num = (3)
 
+#Process
+added_quizes = quiz1 + quiz2 + quiz3
+quiz_averages = added_quizes / num
+
+# Output
+print(" Hello " + name + " your average is " + str(quiz_averages) + ".")
 
 
 
 
 # Practice B
+
+# Input 
+name = (input("Please enter your first name: "))
+distance_drove = float(input("How many miles did you drive?: "))
+gallons_gas = float(input("How many gallons of gas did you use?: "))
+
+# Process
+miles_per_gallon = distance_drove / gallons_gas
+
+# Output
+
+print(" Hello " + name + " you used " + str (miles_per_gallon) + " per gallon of gas on your trip. ")
